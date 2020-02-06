2ba477468075a04c3092e97432c27392f9d4bc56
Michael Angerman <stormasm@gmail.com>
2020-02-05 19:53:19 -0800

    mw

--- td01/src/commands/mod.rs
+++ td01/src/commands/mod.rs
@@ -1,7 +1,2 @@
-mod create;
-mod index;
-mod search;
-
-pub use self::create::run_create_cli;
-pub use self::index::run_index_cli;
-pub use self::search::run_search_cli;
+mod time;
+pub use self::time::run_time_cli;
--- td01/src/main.rs
+++ td01/src/main.rs
@@ -6,56 +6,26 @@ use self::commands::*;
 pub const TANINDEX: &str = "/tmp/tantivy/idxhn";
 
 fn main() {
-    let index_arg = Arg::with_name("index")
-        .short("i")
-        .long("index")
-        .value_name("directory")
-        .help("Tantivy index directory filepath")
-        .required(false);
-
     let cli_options = App::new("Tantivy Hackernews")
         .setting(AppSettings::SubcommandRequiredElseHelp)
         .version(env!("CARGO_PKG_VERSION"))
         .author("Michael Angerman <stormasm@gmail.com>")
         .about("Tantivy Search for Hackernews.")
         .subcommand(
-            SubCommand::with_name("create")
-                .about("Create a new index directory for the hacker news schema")
-                .arg(index_arg.clone()),
-        )
-        .subcommand(
-            SubCommand::with_name("index")
-                .about("Index files")
-                .arg(index_arg.clone())
-                .arg(
-                    Arg::with_name("file")
-                        .short("f")
-                        .long("file")
-                        .value_name("file")
-                        .help("File containing the documents to index."),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("search")
-                .about("Search an index.")
-                .arg(index_arg.clone())
-                .arg(
-                    Arg::with_name("query")
-                        .short("q")
-                        .long("query")
-                        .value_name("query")
-                        .help("Query")
-                        .required(true),
-                ),
+            SubCommand::with_name("time").about("Index files").arg(
+                Arg::with_name("file")
+                    .short("f")
+                    .long("file")
+                    .value_name("file")
+                    .help("File containing the documents to index."),
+            ),
         )
         .get_matches();
 
     let (subcommand, some_options) = cli_options.subcommand();
     let options = some_options.unwrap();
     let run_cli = match subcommand {
-        "create" => run_create_cli,
-        "index" => run_index_cli,
-        "search" => run_search_cli,
+        "time" => run_time_cli,
         _ => panic!("Subcommand {} is unknown", subcommand),
     };
 
