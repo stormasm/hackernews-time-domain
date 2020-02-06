a1e4816c46aeb4e762564acbb10066b189aba043
Michael Angerman <stormasm@gmail.com>
2020-02-05 20:18:11 -0800

    cc

--- td01/src/commands/time.rs
+++ td01/src/commands/time.rs
@@ -1,6 +1,6 @@
-use std::fs::{create_dir, remove_dir_all, File};
+use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::path::Path;
+//use std::path::Path;
 use std::path::PathBuf;
 
 use std::string::String;
@@ -10,50 +10,15 @@ use clap::ArgMatches;
 use crossbeam::crossbeam_channel::{unbounded, Receiver};
 use serde::{Deserialize, Serialize};
 
-use crate::TANINDEX;
-use tantivy::schema::Field;
-use tantivy::schema::*;
-use tantivy::{doc, Index, IndexWriter};
+//use crate::TANINDEX;
+//use tantivy::schema::Field;
+//use tantivy::schema::*;
+//use tantivy::{doc, Index, IndexWriter};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Item {
     id: u64,
     time: u64,
-}
-
-fn add_my_doc(
-    index_writer: &mut IndexWriter,
-    field_id: Field,
-    field_title: Field,
-    id: u64,
-    title: &str,
-) {
-    let doc = doc!(field_title => title, field_id => id);
-    index_writer.add_document(doc);
-}
-
-fn create_schema() -> Schema {
-    let mut schema_builder = Schema::builder();
-    schema_builder.add_text_field("title", TEXT | STORED);
-    schema_builder.add_u64_field("id", FAST | STORED);
-    schema_builder.build()
-}
-
-fn create_index() -> tantivy::Result<Index> {
-    let schema = create_schema();
-
-    let check_path = Path::new(TANINDEX);
-    let dir_exists = check_path.exists();
-    if dir_exists {
-        remove_dir_all(check_path).expect("dir does not exist");
-    }
-
-    let index_path = Path::new(TANINDEX);
-    create_dir(index_path).expect("dir already exists");
-
-    let index = Index::create_in_dir(&index_path, schema.clone())?;
-
-    Ok(index)
 }
 
 fn process_lines(r: Receiver<String>) {
@@ -93,10 +58,10 @@ pub fn run_time_cli(argmatch: &ArgMatches) -> Result<(), String> {
 
     let filename = pb.to_str().unwrap().to_string();
 
-    index_file(filename).map_err(|e| format!("Indexing file failed : {:?}", e))
+    show_time(filename).map_err(|e| format!("Indexing file failed : {:?}", e))
 }
 
-fn index_file(filename: String) -> tantivy::Result<()> {
+fn show_time(filename: String) -> tantivy::Result<()> {
     let _contents = read_file_to_buffer(filename.to_string());
     Ok(())
 }
